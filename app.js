3fb799bd4b3ada28427d021384a397b3558c4605
Daniel Treacy <daniel.t.treacy@gmail.com>
2011-09-14 16:15:01 +1000

    Allowing Heroku to set port in production

--- app.js
+++ app.js
@@ -35,5 +35,7 @@ app.get('/', function(req, res){
   });
 });
 
-app.listen(3000);
+var port = process.env.PORT || 3000;
+
+app.listen(port);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
