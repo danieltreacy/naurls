e4e7f9094b3040b1893c448d47ece3a1f7533e91
Daniel Treacy <daniel.t.treacy@gmail.com>
2011-09-15 14:40:09 +1000

    jQuery events

---
+++ public/javascripts/app.js
@@ -0,0 +1,30 @@
+$(document).ready(function() { 
+
+	$('#source').focus();
+	
+	var shorten = function() {
+		$.ajax({
+			url: 'shorten',
+			data: { u: $('#source').val() },
+			type: 'GET',
+			beforeSend: function() { 
+				$('#preloader').show();
+			},
+			success: function(json) { 
+				$('#source').val('');
+				$('#preloader').hide();
+				$('span.unique').text(json.key);
+				$('#short').attr('href', '/' + json.key);
+				$('#short').fadeIn();
+			}
+		})
+	}
+	
+	
+	$('#source').keypress(function(e) { 
+		if (e.which == 13) {
+			shorten();
+		}
+	});
+		
+});
