4ce29c27fb8375e30652d7da1341719f84852e20
Daniel Treacy <daniel.t.treacy@gmail.com>
2011-09-15 15:04:06 +1000

    Changing Redis config again

--- shortener.js
+++ shortener.js
@@ -3,7 +3,9 @@ var redis;
 var valid_url_pattern = new RegExp(/https?:\/\/([-\w\.]+)+(:\d+)?(\/([\w/_\.]*(\?\S+)?)?)?/);
 
 if (process.env.REDISTOGO_URL) {
-	redis = require('redis-url').connect(process.env.REDISTOGO_URL);
+	var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+	var redis = require("redis").createClient(rtg.port, rtg.hostname);
+	redis.auth(rtg.auth.split(":")[1]);
 } else {
 	redis = require('redis-url').createClient();
 }
