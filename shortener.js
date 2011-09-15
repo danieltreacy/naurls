727a96fe0ae3443bb304cb3de735d4195bfe9540
Daniel Treacy <daniel.t.treacy@gmail.com>
2011-09-15 14:58:36 +1000

    Changed Redis settings for production

--- shortener.js
+++ shortener.js
@@ -3,9 +3,7 @@ var redis;
 var valid_url_pattern = new RegExp(/https?:\/\/([-\w\.]+)+(:\d+)?(\/([\w/_\.]*(\?\S+)?)?)?/);
 
 if (process.env.REDISTOGO_URL) {
-	var rtg   = require('url').parse(process.env.REDISTOGO_URL);
-	redis = require('redis-url').createClient(rtg.port, rtg.hostname);
-	redis.auth(rtg.auth.split(":")[1]);
+	redis = require('redis-url').connect(process.env.REDISTOGO_URL);
 } else {
 	redis = require('redis-url').createClient();
 }
